175cd3becc307e3d25eb92e7881a0e0646d33317
netashawang <wei.wang2@umassmed.edu>
2013-07-22 15:13:11 -0400

    format

--- run_analysis.sh
+++ run_analysis.sh
@@ -35,7 +35,7 @@ OPTIONS:
 	-h      Show this message
 	-i      Input file in bed/norm.bed format, with full directory
 	-o      Output directory, default: current directory
-	-t		input file format
+	-t		input file format:bed or normbed
 	-c      Number of CPUs to use, default: 8
 	-f      Configure file to provide/overwrite variables	
 
