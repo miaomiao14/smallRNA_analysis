fd876ac302c8c1498a6599fa5945bab4910e559a
netashawang <wei.wang2@umassmed.edu>
2013-07-24 18:21:45 -0400

    count

---
+++ total_species_reads_count.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+INDIR=$1
+fileEND=$2
+LOG=${INDIR}/species.reads.stat
+paraFile=${INDIR}/species.reads.stat.${RANDOM}.paraFile
+for i in `ls *.${fileEND}`
+do 
+echo -ne "wc -l ${i} >> $LOG &&" >> ${paraFile}
+echo -e "awk '{a+=\$2}END{print a}' ${i} >> $LOG" >> ${paraFile}
+done
+if [[ ! -f ${paraFile}.completed ]] || [[ -f $$paraFile.failed_commands ]]
+then
+	ParaFly -c $paraFile -CPU 8 -failed_cmds $paraFile.failed_commands
+fi
