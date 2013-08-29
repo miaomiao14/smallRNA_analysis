1c99a8dfdf1b429ee7a3bd60ce2e41ea2e055255
netashawang <wei.wang2@umassmed.edu>
2013-08-28 21:12:21 -0400

    -f

--- run_transposon_piRNA_analysis.sh
+++ run_transposon_piRNA_analysis.sh
@@ -531,7 +531,7 @@ do
 		gt=${gt}" "${name}
 	done 
 	echo -e "echo $gt >${OUTDIR11}/colnames.txt  && " >>${paraFile}
-	echo -e "~hanb/bin2/myMerge -i $tar -o ${OUTDIR11}/species.merged.${i} -c 1 -t 2 -d 0  && " >>${paraFile} 
+	echo -e "[ ! -f ${OUTDIR11}/species.merged.${i} ] && ~hanb/bin2/myMerge -i $tar -o ${OUTDIR11}/species.merged.${i} -c 1 -t 2 -d 0  && " >>${paraFile} 
 	echo -e "cat ${OUTDIR11}/colnames.txt |awk '{ORS=\"\\\t\";OFS=\"\\\t\";FS=\" \"}{ for(i=1;i<=NF; i++) print \$i}' |awk '{ORS=\"\\\n\"} {print}' \>${OUTDIR11}/colnames.txt.temp && " >>${paraFile}
 	echo -e "cat ${OUTDIR11}/colnames.txt.temp ${OUTDIR11}/species.merged.${i} >${OUTDIR11}/species.merged.${i}.txt" >>${paraFile} 
 done
