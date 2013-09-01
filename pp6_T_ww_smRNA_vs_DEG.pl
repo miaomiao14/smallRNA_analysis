adfdc21a392eb81d42a160908e29e65e37e8db5c
netashawang <wei.wang2@umassmed.edu>
2013-09-01 13:49:11 -0400

    no comma when print in perl

--- pp6_T_ww_smRNA_vs_DEG.pl
+++ pp6_T_ww_smRNA_vs_DEG.pl
@@ -28,7 +28,7 @@ for ($i=0; $i<$ARGV[2]; $i++) {
    {print "$filename1-$filename2";
    	open PPSEQ, ">$ARGV[3]/$filename1.$filename2.ppseq";
    	open PPSCORE, ">$ARGV[3]/$filename1.$filename2.ppscore";
-   	#print PPSCORE, "$filename1-$filename2";
+   	#print PPSCORE "$filename1-$filename2";
    }
    #total transposon piRNAs
    $totalReads1=0;
@@ -136,7 +136,7 @@ for ($i=0; $i<$ARGV[2]; $i++) {
          $ppRead2+=$pos{$_} if ($n==10);
          }
       }
-     print PPSCORE, "$n\t$score{$n}\n";
+     print PPSCORE "$n\t$score{$n}\n";
      $score{$n}=0 if (!exists $score{$n});
      if ($n==10) { $X=$score{$n}; delete $score{$n};}
      }
