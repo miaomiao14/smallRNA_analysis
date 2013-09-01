2875ec238aab9e3121c230eb4509f477e1c5cd5c
netashawang <wei.wang2@umassmed.edu>
2013-09-01 10:23:20 -0400

    fix && \

--- run_pp6_FB_smRNA_vs_Degradome.sh
+++ run_pp6_FB_smRNA_vs_Degradome.sh
@@ -13,21 +13,24 @@ declare -a GROUPGT=("ago3MutsWW" "aubvasAgo3CDrescue" "aubvasAgo3WTrescue" "aubM
 #do
 	[ ! -d ${smRNAINDIR}/pp6_FB_smRNA_vs_degradome/${g} ] && mkdir -p ${smRNAINDIR}/pp6_FB_smRNA_vs_degradome/${g}
 	OUTDIR=${smRNAINDIR}/pp6_FB_smRNA_vs_degradome/${g}
+	LOG=${OUTDIR}/${g}.log
 	smmapper2=${smRNAINDIR}/Phil.SRA.${g}.ox.ovary.inserts/Phil.SRA.${g}.ox.ovary.inserts.xkxh.transposon.mapper2.gz
 	demapper2=${degraINDIR}/Phil.DEG.${g}.ovary.PE/bedIntersectWW/Phil.DEG.${g}.ovary.x_rRNA.dm3.sorted.f0x40.noS.5p.all.bed.ntm.collapse.xkxh.FLY_TRANSPOSON_ALL.nta.mapper2.gz
 	[ ! -f $demapper2 ] && \
 		mv ${degraINDIR}/Phil.DEG.${g}.ovary.PE/bedIntersectWW/Phil.DEG.${g}.ovary.x_rRNA.dm3.sorted.f0x40.noS.5p.all.bed.ntm.collapse.FLY_TRANSPOSON_ALL.nta.mapper2.gz $demapper2
-	
+	echo -e "`date` "+$ISO_8601"\tChanged the name of degradome transposon mapper2..." >> $LOG
 	#length range: 23-29
 	[ ! -f ${smmapper2%.gz}.23-29.gz ] && \
 	${PIPELINE_DIRECTORY}/gzlenrangeselector.pl ${smmapper2} 23 29 >${smmapper2%.gz}.23-29 && \
 	gzip ${smmapper2%.gz}.23-29
+	echo -e "`date` "+$ISO_8601"\tSize select the smallRNA transposon mapper2 and gzip it..." >> $LOG
 	#total Ping-Pong
-	[ ! -f ${smRNAINDIR}/pp6_FB_smRNA_vs_degradome/${g}.total.pp6.out ] && $script ${smmapper2%.gz}.23-29.gz ${demapper2} 2 ${smRNAINDIR}/pp6_FB_smRNA_vs_degradome >${smRNAINDIR}/pp6_FB_smRNA_vs_degradome/${g}.total.pp6.out &&
+	[ ! -s ${smRNAINDIR}/pp6_FB_smRNA_vs_degradome/${g}.total.pp6.out ] && $script ${smmapper2%.gz}.23-29.gz ${demapper2} 2 ${smRNAINDIR}/pp6_FB_smRNA_vs_degradome >${smRNAINDIR}/pp6_FB_smRNA_vs_degradome/${g}.total.pp6.out && \
+	echo -e "`date` "+$ISO_8601"\ttotal Ping-Pong analysis done..." >> $LOG
 	
-	
-	${PIPELINE_DIRECTORY}/FB.pl ${smmapper2%.gz}.23-29.gz ${OUTDIR} &&
-	${PIPELINE_DIRECTORY}/FB.pl ${demapper2} ${OUTDIR}	&& 
+	[ ! -s ${OUTDIR}/Phil.SRA.${g}.ox.ovary.inserts.xkxh.transposon.mapper2.23-29.roo ] && ${PIPELINE_DIRECTORY}/FB.pl ${smmapper2%.gz}.23-29.gz ${OUTDIR}
+	[ ! -s ${OUTDIR}/Phil.DEG.${g}.ovary.x_rRNA.dm3.sorted.f0x40.noS.5p.all.bed.ntm.collapse.xkxh.FLY_TRANSPOSON_ALL.nta.mapper2.roo ] && ${PIPELINE_DIRECTORY}/FB.pl ${demapper2} ${OUTDIR} && \
+	echo -e "`date` "+$ISO_8601"\tsplit transposon mapper2 to trns..." >> $LOG
 	paraFile=${OUTDIR}/${g}.pp6.para
 	for j in `ls -1 ${OUTDIR}/Phil.SRA.${g}.ox.ovary.inserts.xkxh.transposon.mapper2.23-29.*`
 	do
@@ -41,5 +44,6 @@ declare -a GROUPGT=("ago3MutsWW" "aubvasAgo3CDrescue" "aubvasAgo3WTrescue" "aubM
 	
 		ParaFly -c $paraFile -CPU $c -failed_cmds $paraFile.failed_commands
 	fi
+	echo -e "`date` "+$ISO_8601"\ttrn PP done..." >> $LOG
 	awk '{OFS="\t"}{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10}' ${OUTDIR}/${g}.FB.pp6.temp > ${OUTDIR}/${g}.FB.pp6
 #done
