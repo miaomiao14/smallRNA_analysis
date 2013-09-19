0f313cba2677d173f19681ad545cc71b4104ed77
netashawang <wei.wang2@umassmed.edu>
2013-09-19 16:55:46 -0400

    sense_correlation_analysis

--- run_correlation_analysis.sh
+++ run_correlation_analysis.sh
@@ -48,12 +48,26 @@ do
 	done
 done
 
-OUTDIR2=${INDIR}/smRNA/correlation_analysis
+#OUTDIR2=${INDIR}/smRNA/correlation_analysis
+#[ ! -d ${OUTDIR2} ] && mkdir -p ${OUTDIR2}
+#paraFile=${OUTDIR2}/RSQDEGSRA.correlationanalysis.${RANDOM}.para && \
+#for g in "${GROUPGT[@]}"
+#do
+#	SRA=${INDIR}/smRNA/diff_nalysis/Phil.SRA.${g}.ox.nnc.normlized.transposonpiRNAs.list.txt
+#	RSQ=${INDIR}/rnaseq/diff_analysis2/${g}/${g}.x_rRNA.dm3.Aligned.out.FLY_TRANSPOSON_ALL_GENE.htseqcount.nf.out.DESeqNF.transposon.normalizedcounts.txt
+#	DEG=${INDIR}/degradome/diff_analysis2/${g}/${g}.x_rRNA.dm3.Aligned.out.FLY_TRANSPOSON_ALL_GENE.htseqcount.nf.out.DESeqNF.transposon.normalizedcounts.txt
+#	
+#	echo -e "${PIPELINE_DIRECTORY}/RRR ${PIPELINE_DIRECTORY}/R.source plot_correlation $SRA $RSQ $DEG ${g} ${OUTDIR2}" >>${paraFile}	
+#done
+#ParaFly -c $paraFile -CPU 8 -failed_cmds $paraFile.failed_commands 
+
+
+OUTDIR2=${INDIR}/smRNA/sense_correlation_analysis
 [ ! -d ${OUTDIR2} ] && mkdir -p ${OUTDIR2}
 paraFile=${OUTDIR2}/RSQDEGSRA.correlationanalysis.${RANDOM}.para && \
 for g in "${GROUPGT[@]}"
 do
-	SRA=${INDIR}/smRNA/diff_nalysis/Phil.SRA.${g}.ox.nnc.normlized.transposonpiRNAs.list.txt
+	SRA=${INDIR}/smRNA/diff_nalysis/Phil.SRA.${g}.ox.nnc.normlized.S.transposonpiRNAs.list.txt
 	RSQ=${INDIR}/rnaseq/diff_analysis2/${g}/${g}.x_rRNA.dm3.Aligned.out.FLY_TRANSPOSON_ALL_GENE.htseqcount.nf.out.DESeqNF.transposon.normalizedcounts.txt
 	DEG=${INDIR}/degradome/diff_analysis2/${g}/${g}.x_rRNA.dm3.Aligned.out.FLY_TRANSPOSON_ALL_GENE.htseqcount.nf.out.DESeqNF.transposon.normalizedcounts.txt
 	
