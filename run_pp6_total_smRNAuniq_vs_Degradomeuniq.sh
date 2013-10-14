78c5e04912330962086439f5e812f98203b049fa
netashawang <wei.wang2@umassmed.edu>
2013-10-14 17:33:49 +0800

    8

--- run_pp6_total_smRNAuniq_vs_Degradomeuniq.sh
+++ run_pp6_total_smRNAuniq_vs_Degradomeuniq.sh
@@ -25,6 +25,6 @@ do
 	gzip ${smmapper2%.gz}.23-29
 	echo -e "`date` "+$ISO_8601"\tSize select the smallRNA transposon mapper2 and gzip it..." >> $LOG
 	#total Ping-Pong
-	[ ! -s ${smRNAINDIR}/pp6_FB_smRNAuniq_vs_degradomeuniq_total_with_ppscore/${g}.total.pp6.out ] && submitsge 24 $g "$script ${smmapper2%.gz}.23-29.gz ${demapper2} 2 ${smRNAINDIR}/pp6_FB_smRNAuniq_vs_degradomeuniq_total_with_ppscore >${smRNAINDIR}/pp6_FB_smRNAuniq_vs_degradomeuniq_total_with_ppscore/${g}.uniqmap.total.pp6.out" 
+	[ ! -s ${smRNAINDIR}/pp6_FB_smRNAuniq_vs_degradomeuniq_total_with_ppscore/${g}.total.pp6.out ] && submitsge 8 $g "$script ${smmapper2%.gz}.23-29.gz ${demapper2} 2 ${smRNAINDIR}/pp6_FB_smRNAuniq_vs_degradomeuniq_total_with_ppscore >${smRNAINDIR}/pp6_FB_smRNAuniq_vs_degradomeuniq_total_with_ppscore/${g}.uniqmap.total.pp6.out" 
 	#echo -e "`date` "+$ISO_8601"\ttotal Ping-Pong analysis done..." >> $LOG
 done
