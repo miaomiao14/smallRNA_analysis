f89d737b1b7500204fca81d58bd4002d08820f1d
netashawang <wei.wang2@umassmed.edu>
2013-10-22 02:18:01 -0400

    cluster

--- submit_cluster_bucket_ww.sh
+++ submit_cluster_bucket_ww.sh
@@ -24,8 +24,9 @@ echo "#!/bin/sh
 #$ -V
 #$ -pe single 8
 #$ -o \$HOME/sge_jobs_output/sge_job.\$JOB_ID.out -j y
-#$ -S /bin/bash
 #$ -l mem_free=31G
+#$ -S /bin/bash
+
 
 export PIPELINEDIR=/home/xuj1/nearline/cluster/data:/home/xuj1/pipeline:/home/xuj1/pipeline/bowtie:/home/lees2/pipeline:
 export OUTPUTDIR=$6
