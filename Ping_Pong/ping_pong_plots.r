ce7975fd0a6a9171f35cb4ea13c1c0027f7633c4
netashawang <wei.wang2@umassmed.edu>
2013-11-08 16:19:58 -0500

    a

--- Ping_Pong/ping_pong_plots.r
+++ Ping_Pong/ping_pong_plots.r
@@ -21,8 +21,10 @@ plot_ua_va <- function (input,gt,outdir) {
 		fun=function (x) {if (x == "AU" | x == "CG" | x == "GC" | x=="UA") {y=1} else { y=0}}
 		ff<-ddply(f,"pair",transform,group=fun(pair))
 		ff$group=as.factor(ff$group)
-		f1=subset(ff,group==1)
-		f2=subset(ff,group==0)
+		f1=as.data.frame(lapply( subset(f,as.character(group)==1),'[',drop=TRUE))
+		f2=as.data.frame(lapply( subset(f,as.character(group)==0),'[',drop=TRUE))
+		#f1=subset(ff,group==1)
+		#f2=subset(ff,group==0)
 		#############################################
 		#b<-barplot(f1$NofPairs,names.arg=f1$pair,main=m,axes=F,ylim=c(0,1.2*max(f1$NofPairs)), col=rep("darkblue",4),border="white",cex.names=1,ylab="Paired Number of pairs")
 		#text(x=b,y=f1$NofPairs+2.5,label=f1$raw,cex=1)
@@ -35,8 +37,7 @@ plot_ua_va <- function (input,gt,outdir) {
 		#grid(nx=NA,ny=NULL,lty=1,lwd=1,col="gray")
 		#axis(4,ylim=c(0,1.2*max(f2$NofPairs)))
 		#############################################
-		
-		
+			
 		# data a
 		a=seq(1,8,2)
 	# data b, different scale
@@ -54,14 +55,13 @@ plot_ua_va <- function (input,gt,outdir) {
 	# draw X axis
 		axis (1,tick=0.01,las=1)
 	# draw left Y axis
-		axis (2, ylim = c(0, 1.2*max(f1$NofPairs)),  at = seq (0, 1.2*max(f1$NofPairs), as.integer ( 1.2*max(f1$NofPairs)/5 ) ) )Ê
+		axis (2, ylim = c(0, 1.2*max(f1$NofPairs)),  at = seq (0, 1.2*max(f1$NofPairs), as.integer ( 1.2*max(f1$NofPairs)/5 ) ) )
 	# draw data b for its scale
 		plot.window (xlim=c(0,1.2*max(a)), ylim=c(0, 1.2*max(f2$NofPairs)))
 	# draw data b, distance between two differents data sesÊ
 		rect (xleft=a+bar_dis-bar_width/2, xright=a+bar_dis+bar_width/2, ybottom=0, ytop=f2$NofPairs, ylim = c(0, 1.2*max(f2$NofPairs)), col="darkgrey", border=F )
 		axis (4, ylim = c(0, 1.2*max(f2$NofPairs)), at = seq (0, 1.2*max(f2$NofPairs), as.integer (1.2*max(f2$NofPairs)/5)))
-		
-		
+	
 	}
 	dev.off()	
 }
