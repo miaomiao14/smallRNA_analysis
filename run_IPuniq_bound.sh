936ede59b9ecb0c7354953b45591fb69c97d15b2
netashawang <wei.wang2@umassmed.edu>
2013-11-08 11:58:21 -0500

    ${parafile}

--- run_IPuniq_bound.sh
+++ run_IPuniq_bound.sh
@@ -10,6 +10,7 @@ declare -a GT=("w1" "AubCDrescue" "AubWTrescue" "aubvasAgo3CDrescue" "aubvasAgo3
 declare -a OX=("ox" "unox")
 echo -e "genotype\tox\tsharedSpecies\tsharedReadsAgo3IP\tsharedReadsAubIP\tuniqSpeciesAgo3IP\tuniqSpeciesAubIP\tuniqReadsAgo3IP\tuniqReadsAubIP\n" >> ${OUTPUTDIR}/${g}.${o}.log
 parafile=${OUTPUTDIR}/para.uniq.bound
+[ -s ${parafile} ] && rm ${parafile}
 for g in "${GT[@]}"
 do
 	for o in "${OX[@]}"
@@ -17,22 +18,22 @@ do
 	cd ${OUTPUTDIR}	
 	echo -ne " A=Phil.SRA.Ago3IP.${g}.${o}.ovary.trimmed && " >>${parafile}
 	echo -ne " B=Phil.SRA.AubIP.${g}.${o}.ovary.trimmed && " >>${parafile}
-	echo -ne " ln -s ${INPUTDIR}/${A} " >>${parafile}
-	echo -ne " ln -s ${INPUTDIR}/${B} " >>${parafile}
+	echo -ne " ln -s ${INPUTDIR}/\${A} " >>${parafile}
+	echo -ne " ln -s ${INPUTDIR}/\${B} " >>${parafile}
  
-	echo -ne " /home/wangw1/bin/inserts_file_methods.py -a $A -b $B -i && " >>${parafile}
-	echo -ne " sharedSpecies=`wc -l ${A}.sharedA|cut -d" " -f1` && " >>${parafile}
-	echo -ne " sharedReadsAgo3IP=`sumcol ${A}.sharedA 2` && " >>${parafile}
-	echo -ne " sharedReadsAubIP=`sumcol ${B}.sharedB 2` && " >>${parafile}
+	echo -ne " /home/wangw1/bin/inserts_file_methods.py -a \$A -b \$B -i && " >>${parafile}
+	echo -ne " sharedSpecies=`wc -l \${A}.sharedA|cut -d" " -f1` && " >>${parafile}
+	echo -ne " sharedReadsAgo3IP=\`sumcol \${A}.sharedA 2\` && " >>${parafile}
+	echo -ne " sharedReadsAubIP=\`sumcol \${B}.sharedB 2\` && " >>${parafile}
 	
-	echo -ne " A=Phil.SRA.Ago3IP.${g}.${o}.ovary.trimmed && " >>${parafile}
-	echo -ne " B=Phil.SRA.AubIP.${g}.${o}.ovary.trimmed && " >>${parafile}
+	#echo -ne " A=Phil.SRA.Ago3IP.${g}.${o}.ovary.trimmed && " >>${parafile}
+	#echo -ne " B=Phil.SRA.AubIP.${g}.${o}.ovary.trimmed && " >>${parafile}
 	echo -ne "/home/wangw1/bin/inserts_file_methods.py -a $A -b $B -u && " >>${parafile}
 	
-	echo -ne " uniqSpeciesAgo3IP=`wc -l ${A}.uniqA` && " >>${parafile}
-	echo -ne " uniqSpeciesAubIP=`wc -l ${B}.uniqB` && " >>${parafile}
-	echo -ne " uniqReadsAgo3IP=`sumcol ${A}.uniqA 2` && " >>${parafile} 
-	echo -ne " uniqReadsAubIP=`sumcol ${B}.uniqB 2` && " >>${parafile}
+	echo -ne " uniqSpeciesAgo3IP=\`wc -l \${A}.uniqA\` && " >>${parafile}
+	echo -ne " uniqSpeciesAubIP=\`wc -l \${B}.uniqB\` && " >>${parafile}
+	echo -ne " uniqReadsAgo3IP=\`sumcol \${A}.uniqA 2\` && " >>${parafile} 
+	echo -ne " uniqReadsAubIP=\`sumcol \${B}.uniqB 2\` && " >>${parafile}
 
 	echo -e " echo -e \"\${g}\\t\${o}\\t\${sharedSpecies}\\t\${sharedReadsAgo3IP}\\t\${sharedReadsAubIP}\\t\${uniqSpeciesAgo3IP}\\t\${uniqSpeciesAubIP}\\t\${uniqReadsAgo3IP}\\t\${uniqReadsAubIP}\\n\" \>\> \${OUTPUTDIR}/\${g}.\${o}.log " >>${parafile}
 
