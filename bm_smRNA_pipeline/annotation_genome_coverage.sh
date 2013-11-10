d20adf80fe5218c13cc291b330b3743d9228ec4f
netashawang <wei.wang2@umassmed.edu>
2013-11-09 20:33:14 -0500

    1

--- bm_smRNA_pipeline/annotation_genome_coverage.sh
+++ bm_smRNA_pipeline/annotation_genome_coverage.sh
@@ -14,7 +14,7 @@ awk '{OFS="\t"}{print $1,0,$2-1}' $CHRSIZE >${OUTDIR}/${prefix}.chrsize.bed
 for i in `ls ${INDIR}/*.bed`
 do
 bedtools coverage -a ${i} -b ${OUTDIR}/${prefix}.chrsize.bed > ${OUTDIR}/${i}.coverage
-echo -e "${i}\tGenomeSize\tCoverageFraction" >> $LOG
+echo -e "${i##*/}\tGenomeSize\tCoverageFraction" >> $LOG
 awk '{a=a+$5;b=b+$6;}END{print a,b,a/b}' ${OUTDIR}/${i}.coverage >> $LOG
 
 done
