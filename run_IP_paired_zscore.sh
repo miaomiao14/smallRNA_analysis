b03320da0b3b4a98f05f0f6ec9c085be5e71461f
netashawang <wei.wang2@umassmed.edu>
2013-11-14 20:16:01 -0500

    1

--- run_IP_paired_zscore.sh
+++ run_IP_paired_zscore.sh
@@ -180,6 +180,30 @@ do
 		done
 	done		
 done
+for t in ${GT[@]}
+do
+	for o in ${OX[@]}
+	do
+
+			for pp in ${PPPAIR[@]}
+			do
+				fn=${OUTDIR}/${t}_${o}_${pp}
+				file=${OUTDIR}/${t}_${o}_${pp}
+				[ -f ${file}.pair.count.txt ] &&  rm ${file}.pair.count.txt
+				for i in `ls ${fn}/*.UA_VA.zscore.out`
+				do
+					/home/wangw1/git/smallRNA_analysis/Ping_Pong/UA_VA_rawscore.pl ${i} $OUTDIR >> ${file}.pair.count.txt
+				done
+				#sort -k1,1 -k2,2 -k3,3 -k4,4 $file.pair.count.txt | uniq >${file}.pair.count.uniq.txt
+				RRR /home/wangw1/git/smallRNA_analysis/Ping_Pong/ping_pong_plots.r plot_ua_va ${file}.pair.count.uniq.txt ${t}_${o}_${pp} ${SUMMARYOUTDIR}
+				RRR /home/wangw1/git/smallRNA_analysis/Ping_Pong/ping_pong_plots.r plot_ua_va_color ${file}.pair.count.uniq.txt ${t}_${o}_${pp} ${SUMMARYOUTDIR}
+			done
+
+	done		
+done
+
+
+
 [ $? == 0 ] && \
 touch .status.${STEP}.pp8_UA_VA_summary
 
