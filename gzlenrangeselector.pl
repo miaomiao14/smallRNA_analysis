783aa96591868f47bb79fefcdc9121ce15e067a9
netashawang <wei.wang2@umassmed.edu>
2014-02-16 16:10:48 -0500

    1

--- gzlenrangeselector.pl
+++ gzlenrangeselector.pl
@@ -7,11 +7,17 @@ use Compress::Zlib;
 if(@ARGV<1) { print "USAGE:gzlenrangeselector.pl <file> <len1> <len2> \n"; exit 1;}
 #open IN, $ARGV[0];
 $gz = gzopen($ARGV[0], "rb") or die "Cannot open $ARGV[0]: $gzerrno\n" ;
+$infile=$ARGV[0];
+$infile=~s/.gz//;
+$outfile=$infile.$ARGV[1];
+$outfile=$outfile.$ARGV[2];
+$gzw=gzopen($outfile,"wb") or die "Cannot open $outfile: $gzerrno\n";
 while($gz->gzreadline($line) > 0)
 {
  #$line=$_;
  chomp $line;
  @l=split(/\t/,$line);
- print "$line\n" if (length($l[0])>=$ARGV[1] & length($l[0])<=$ARGV[2]);
+ $gzw->gzwrite( "$line\n") if (length($l[0])>=$ARGV[1] & length($l[0])<=$ARGV[2]);
 } 
-$gz->gzclose(); 
+$gz->gzclose();
+$gzw->gzclose();  
