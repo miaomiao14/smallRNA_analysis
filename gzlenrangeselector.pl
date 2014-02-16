0f9018ddd21a45d37c26a73eaa746879ae071870
netashawang <wei.wang2@umassmed.edu>
2014-02-16 15:40:31 -0500

    $_

--- gzlenrangeselector.pl
+++ gzlenrangeselector.pl
@@ -4,11 +4,12 @@
 #select a range of length of the seqs 
 #lenselector <file> <len min> <len max>
 use Compress::Zlib;
-if(@ARGV<1) { print "USAGE:lenselector <file> <len1> <len2> \n"; exit 1;}
+if(@ARGV<1) { print "USAGE:gzlenrangeselector.pl <file> <len1> <len2> \n"; exit 1;}
 #open IN, $ARGV[0];
 $gz = gzopen($ARGV[0], "rb") or die "Cannot open $ARGV[0]: $gzerrno\n" ;
-while($bytesread =$gz->gzreadline($line) > 0)
+while($gz->gzreadline($_) > 0)
 {
+ $line=$_;
  chomp $line;
  @l=split(/\t/,$line);
  print "$line\n" if (length($line[0])>=$ARGV[1] & length($line[0])<=$ARGV[2]);
