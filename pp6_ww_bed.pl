09fa61ad4ae89b638e2c4427d373d47f5a6eaf75
netashawang <wei.wang2@umassmed.edu>
2014-02-23 16:17:32 -0500

    1

--- pp6_ww_bed.pl
+++ pp6_ww_bed.pl
@@ -342,9 +342,11 @@ for (my $i=0; $i<@inputfiles; $i++)
 		if ($score{10}) { $X=$score{10}; delete $score{10};} #mean and standard deviation exclude ppscore10
 		my $std=0;
 		$std=&standard_deviation(values %score);
-		if ( (scalar keys (%score))>9 && $std>0) { $Z=($X-&mean(values %score))/$std;} else {$Z=-10;}
+		my $m=0;
+		$m=&mean(values %score);
+		if ( (scalar keys (%score))>9 && $std>0) { $Z=($X-$m)/$std;} else {$Z=-10;}
 		printf PPZ '%.2f', $Z;
-		print PPZ "\t$X\n";
+		print PPZ "\t$X\t$m\t$std\n";
 	}#i
 
 } #j
