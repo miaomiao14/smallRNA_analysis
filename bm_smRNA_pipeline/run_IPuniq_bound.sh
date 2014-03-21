5c89fde3794e044998e8445f953fd8a6118a3e70
netashawang <wei.wang2@umassmed.edu>
2014-03-21 11:48:06 -0400

    1

--- bm_smRNA_pipeline/run_IPuniq_bound.sh
+++ bm_smRNA_pipeline/run_IPuniq_bound.sh
@@ -3,8 +3,8 @@
 
 export PIPELINEDIR=/home/lees2/pipeline:/home/xuj1/pipeline
 
-INPUTDIR=/home/wangw1/isilon_temp/ipsmRNA/trimmed
-OUTPUTDIR=/home/wangw1/isilon_temp/ipsmRNA/trimmed_uniqBoundsds
+INPUTDIR=/home/wangw1/isilon_temp/BmN4/xiorIPprocesseddata
+OUTPUTDIR=/home/wangw1/isilon_temp/BmN4/xiorIPprocesseddata/uniqBound
 [ ! -d $OUTPUTDIR ] && mkdir $OUTPUTDIR
 
 STEP=1
