5480a85beaa9c0d19064986a4656c08d60611efb
netashawang <wei.wang2@umassmed.edu>
2014-04-18 15:34:29 -0400

    1

--- g1Ut10A/run_mouse_processed_pp.sh
+++ g1Ut10A/run_mouse_processed_pp.sh
@@ -19,8 +19,8 @@ do
 	cat $i |grep trans |cut -f1,3,5 |awk 'BEGIN{OFS="\t"}{print $2,$1,$3}' >${OUTDIR}/${filename}.piSpe.txt
 	cat $i |grep trans |cut -f1,3,6 |awk 'BEGIN{OFS="\t"}{print $2,$1,$3}' >${OUTDIR}/${filename}.pairedReadsSpe.txt
 	
-	[ ! -s ${OUTDIR}/masterTable.${filename}.prefixSpe.txt ] && ${PIPELINE_DIRECTORY}/RRR ${PIPELINE_DIRECTORY}/R.source cast_master_table ${OUTDIR}/${filename}.prefixSpe.txt ${OUTDIR}/masterTable.${filename}.prefixSpe.txt && rm ${OUTDIR}/${filename}.prefixSpe.txt
-	[ ! -s ${OUTDIR}/masterTable.${filename}.piSpe.txt ] && ${PIPELINE_DIRECTORY}/RRR ${PIPELINE_DIRECTORY}/R.source cast_master_table ${OUTDIR}/${filename}.piSpe.txt ${OUTDIR}/masterTable.${filename}.piSpe.txt && rm ${OUTDIR}/${filename}.piSpe.txt
-	[ ! -s ${OUTDIR}/masterTable.${filename}.pairedReadsSpe.txt ] && ${PIPELINE_DIRECTORY}/RRR ${PIPELINE_DIRECTORY}/R.source cast_master_table ${OUTDIR}/${filename}.pairedReadsSpe.txt ${OUTDIR}/masterTable.${filename}.pairedReadsSpe.txt && rm ${OUTDIR}/${filename}.pairedReadsSpe.txt
+	[ ! -s ${OUTDIR}/masterTable.${filename}.prefixSpe.txt ] && ${PIPELINE_DIRECTORY}/RRR ${PIPELINE_DIRECTORY}/R.source cast_master_table ${OUTDIR}/${filename}.prefixSpe.txt ${OUTDIR}/masterTable.${filename}.prefixSpePairs.txt && rm ${OUTDIR}/${filename}.prefixSpe.txt
+	[ ! -s ${OUTDIR}/masterTable.${filename}.piSpe.txt ] && ${PIPELINE_DIRECTORY}/RRR ${PIPELINE_DIRECTORY}/R.source cast_master_table ${OUTDIR}/${filename}.piSpe.txt ${OUTDIR}/masterTable.${filename}.SpeciesPairs.txt && rm ${OUTDIR}/${filename}.piSpe.txt
+	[ ! -s ${OUTDIR}/masterTable.${filename}.pairedReadsSpe.txt ] && ${PIPELINE_DIRECTORY}/RRR ${PIPELINE_DIRECTORY}/R.source cast_master_table ${OUTDIR}/${filename}.pairedReadsSpe.txt ${OUTDIR}/masterTable.${filename}.ReadsPairs.txt && rm ${OUTDIR}/${filename}.pairedReadsSpe.txt
 							
 done
