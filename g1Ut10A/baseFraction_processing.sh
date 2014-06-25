cddd8f0a4d738fc6b8967f0f3f888a50f041b227
netashawang <wei.wang2@umassmed.edu>
2014-06-25 18:41:30 -0400

    1

--- g1Ut10A/baseFraction_processing.sh
+++ g1Ut10A/baseFraction_processing.sh
@@ -2,16 +2,25 @@
 
 export PIPELINE_DIRECTORY=/home/wangw1/git/smallRNA_analysis
 
+#to process the output from /home/wangw1/git/smallRNA_analysis/g1Ut10A/pp8_q2_ww1_zscore_sep_0624_full.pl
+#input file: eg. FLAGSiwiIP_KNOWNTE_AS.FLAGSiwiIP_KNOWNTE_AS.10.prefix.UA_VA.ppseq.txt
+#trans	TGCTAGGGTTCGTGTTAGCAACGTCGT	nscaf2564,274886,+	CGTGTTAGCAACG	0.000514668039114771	AACCCTAGCAAGAGTCGTGCTTCGCAGA	scaffold14279,667,-	CGTGTTAGCATCG	0.00607287449392713	1111111111011
+
 INDIR=$1
 OUTDIR=$2
-
 [ ! -f ${OUTDIR} ] && mkdir ${OUTDIR}
+
+declare -a GROUPGT=("cis" "trans" "ambiguous")
 
 for i in ${INDIR}/*.UA_VA.ppseq.txt
 do
 	filename=${i##*/}
-	cat $i |grep trans |cut -f2,3,4,5 |sort -u >${OUTDIR}/${filename%.txt}.guide.
-	${PIPELINE_DIRECTORY}/Utils/base_fraction.pl -i Brennecke.SRA.MTD_zuc.unox.ovary.trimmed.insert -o $PWD -p 1 -r 2 -l 23
-	
-	cat $i |grep trans |cut -f6,7,8,9 |sort -u >
+	for g in "${GROUPGT[@]}"
+	do
+		cat $i |grep ${g} |cut -f2,3,4,5 |sort -u |cut -f1,4 >${OUTDIR}/${filename%.txt}.${g}.guide
+		${PIPELINE_DIRECTORY}/Utils/base_fraction.pl -i ${OUTDIR}/${filename%.txt}.${g}.guide -o ${OUTDIR} -p 1 -r 2 -l 23
+		
+		cat $i |grep ${g} |cut -f6,7,8,9 |sort -u |cut -f1,4 >${OUTDIR}/${filename%.txt}.${g}.target
+		${PIPELINE_DIRECTORY}/Utils/base_fraction.pl -i ${OUTDIR}/${filename%.txt}.${g}.target -o ${OUTDIR} -p 1 -r 2 -l 23
+	done
 done
