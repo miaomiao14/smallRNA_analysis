98a02a91c93bccd46ff4c8e7613262b25b410cca
netashawang <wei.wang2@umassmed.edu>
2014-08-07 15:22:03 -0400

    1

--- primary_piRNA_biogenesis/run_pp8_total_smRNA_vs_smRNA.sh
+++ primary_piRNA_biogenesis/run_pp8_total_smRNA_vs_smRNA.sh
@@ -4,21 +4,6 @@ script=${PIPELINE_DIRECTORY}/pp8_ww_smRNA_vs_DEG.pl
 
 OUT0=/home/bh80w/scratch/SRA/pp8
 
-
-#c=$6 #cpu
-declare -a GROUPGT=("/scratch/hanb/cd/smallRNA_pipeline_output/Brennecke.SRA.WT.unox.OSC/intersect_piRNA_length/Brennecke.SRA.WT.unox.OSC.trimmed.x_rRNA.x_hairpin.x_dmel_virus.dm3v0.uniq.L23mer.x_rpmk_rtRNA.bed2" \
-"/scratch/hanb/cd/smallRNA_pipeline_output/Brennecke.SRA.zucMut.unox.ovary/intersect_piRNA_length/Brennecke.SRA.zucMut.unox.ovary.trimmed.x_rRNA.x_hairpin.x_dmel_virus.dm3v0.uniq.L23mer.x_rpmk_rtRNA.bed2" \
-"/scratch/hanb/cd/smallRNA_pipeline_output/Phil.SRA.Ago3IPsds.aubMuts.unox.ovary/intersect_piRNA_length/Phil.SRA.Ago3IPsds.aubMuts.unox.ovary.trimmed.x_rRNA.x_hairpin.x_dmel_virus.dm3v0.uniq.L23mer.x_rpmk_rtRNA.bed2" \
-"/scratch/hanb/cd/smallRNA_pipeline_output/Phil.SRA.Ago3IPsds.w1.unox.ovary/intersect_piRNA_length/Phil.SRA.Ago3IPsds.w1.unox.ovary.trimmed.x_rRNA.x_hairpin.x_dmel_virus.dm3v0.uniq.L23mer.x_rpmk_rtRNA.bed2" \
-"/scratch/hanb/cd/smallRNA_pipeline_output/Phil.SRA.ago3MutsAubMuts.ox.ovary/intersect_piRNA_length/Phil.SRA.ago3MutsAubMuts.ox.ovary.trimmed.x_rRNA.x_hairpin.x_dmel_virus.dm3v0.uniq.L23mer.x_rpmk_rtRNA.bed2" \
-"/scratch/hanb/cd/smallRNA_pipeline_output/Phil.SRA.ago3Mutsrep2.ox.ovary/intersect_piRNA_length/Phil.SRA.ago3Mutsrep2.ox.ovary.trimmed.x_rRNA.x_hairpin.x_dmel_virus.dm3v0.uniq.L23mer.x_rpmk_rtRNA.bed2" \
-"/scratch/hanb/cd/smallRNA_pipeline_output/Phil.SRA.AubCDrescuerep2.ox.ovary/intersect_piRNA_length/Phil.SRA.AubCDrescuerep2.ox.ovary.trimmed.x_rRNA.x_hairpin.x_dmel_virus.dm3v0.uniq.L23mer.x_rpmk_rtRNA.bed2" \
-"/scratch/hanb/cd/smallRNA_pipeline_output/Phil.SRA.aubMutsrep2.ox.ovary/intersect_piRNA_length/Phil.SRA.aubMutsrep2.ox.ovary.trimmed.x_rRNA.x_hairpin.x_dmel_virus.dm3v0.uniq.L23mer.x_rpmk_rtRNA.bed2" \
-"/scratch/hanb/cd/smallRNA_pipeline_output/Phil.SRA.aubvasAgo3CDrescuerep2.ox.ovary/intersect_piRNA_length/Phil.SRA.aubvasAgo3CDrescuerep2.ox.ovary.trimmed.x_rRNA.x_hairpin.x_dmel_virus.dm3v0.uniq.L23mer.x_rpmk_rtRNA.bed2" \
-"/scratch/hanb/cd/smallRNA_pipeline_output/Phil.SRA.aubvasAgo3WTrescuerep2.ox.ovary/intersect_piRNA_length/Phil.SRA.aubvasAgo3WTrescuerep2.ox.ovary.trimmed.x_rRNA.x_hairpin.x_dmel_virus.dm3v0.uniq.L23mer.x_rpmk_rtRNA.bed2" \
-"/scratch/hanb/cd/smallRNA_pipeline_output/Phil.SRA.AubWTrescuerep2.ox.ovary/intersect_piRNA_length/Phil.SRA.AubWTrescuerep2.ox.ovary.trimmed.x_rRNA.x_hairpin.x_dmel_virus.dm3v0.uniq.L23mer.x_rpmk_rtRNA.bed2" \
-"/scratch/hanb/cd/smallRNA_pipeline_output/Phil.SRA.w1.ox.ovary/intersect_piRNA_length/Phil.SRA.w1.ox.ovary.trimmed.x_rRNA.x_hairpin.x_dmel_virus.dm3v0.uniq.L23mer.x_rpmk_rtRNA.bed2")
-
 declare -a FEATURE=("all")
 
 STEP=1
@@ -47,7 +32,7 @@ do
 		[ ! -f $smmapper2 ] && \
 		awk 'BEGIN{OFS="\t"}{if($3-$2<30) {start=$2+1;print $1,start,$3,$6,$7,$4,$5}}' $i >${smmapper2} && gzip ${smmapper2}.gz	
 	#total Ping-Pong
-		[ ! -s ${OUT0}/${g}.total.pp8.out ] && hm.bsub.wrapper 1 10:00 short ${g} ${OUT0} "$script ${smmapper2}.gz ${smmapper2}.gz 1 ${OUTDIR} >${OUT0}/${g}.total.pp8.out" 
+		[ ! -s ${OUT0}/${g}.total.pp8.out ] && /home/ww74w/smallRNApipeline/bin/hm.bsub.wrapperhm.bsub.wrapper 1 10:00 short ${g} ${OUT0} "$script ${smmapper2}.gz ${smmapper2}.gz 1 ${OUTDIR} >${OUT0}/${g}.total.pp8.out" 
 	done
 
 done
