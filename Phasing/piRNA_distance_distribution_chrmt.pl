597b4f9b7597c39a8e6fd6372578a3f977db2a15
netashawang <wei.wang2@umassmed.edu>
2014-08-11 17:15:26 -0400

    1

--- Phasing/piRNA_distance_distribution_chrmt.pl
+++ Phasing/piRNA_distance_distribution_chrmt.pl
@@ -26,7 +26,7 @@ use Getopt::Std;
 my %Options;
 
 my $numArgs = $#ARGV + 1;
-my $ok = getopts('i:o:F:D:w', \%Options);
+my $ok = getopts('i:o:F:D:w:', \%Options);
 if($numArgs < 4 || !$ok || !exists($Options{i}) || !exists($Options{o}) ) 
 { 
   Usage();
