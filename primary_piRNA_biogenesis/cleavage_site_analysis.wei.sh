6e500dd8d1fd7dc9f8414ac7682585c54e098c62
turningA <wei.wang2@umassmed.edu>
2014-08-19 13:31:10 -0400

    permission

--- primary_piRNA_biogenesis/cleavage_site_analysis.wei.sh
+++ primary_piRNA_biogenesis/cleavage_site_analysis.wei.sh
@@ -1,35 +1,15 @@
 
 ##input files is ppseq.txt from pp8_v24_v2.pl
 input=$1
+outdir=$2
 
-outdir=$3
-##anchor point, extend 100 nt on both end
 
+##cleavage siteas anchor point,  
 ##trans
-grep trans $input |cut -f7 \
-	| awk 'BEGIN{OFS="\t"}{split($1,a,","); if(a[3]=="+"){start=a[2]+10;strand="-";left=start-100;right=start+100; print a[1],left,right,"*","*",strand} else {start=a[2]-9;strand="+";print a[1],left,right,"*","*",strand}}' \
-		|sort -u >${outdir}/${input}.trans.bed
-grep "+" ${input}.trans.bed >${outdir}/${input}.trans.plus.bed
-grep "-" ${input}.trans.bed >${outdir}/${input}.trans.minus.bed
+grep trans $input | awk 'BEGIN{OFS="\t"}{split($7,a,","); if(a[3]=="+"){start=a[2]+10;strand="-"; print a[1],start-1,start,$9,$5*$9,strand} else {start=a[2]-10;strand="+";print a[1],start,start+1,$9,$5*$9,strand}}' | sort -k1,1 -k2,2 -k3,3 -k6,6 |bedtools groupby -i stdin -g 1,2,3,6 -c 4,5 -o sum,sum | sort -k 6,6rn > ${outdir}/${input}.trans.bed &
 ##cis
-grep cis $input |cut -f7 \
-	| awk 'BEGIN{OFS="\t"}{split($1,a,","); if(a[3]=="+"){start=a[2]+10;strand="-"; left=start-100;right=start+100; print a[1],left,right,"*","*",strand} else {start=a[2]-9;strand="+";print a[1],left,right,"*","*",strand}}' \
-		|sort -u >${outdir}/${input}.cis.bed &
-grep "+" ${input}.cis.bed >${outdir}/${input}.trans.plus.bed
-grep "-" ${input}.cis.bed >${outdir}/${input}.trans.minus.bed
-
-##use the above bed intervals as chrsize
-
-normbed=$2
-normFac=1
-zcat $normbed |sed 1d | awk -F "\t" -v norm=$normFac '{OFS="\t"; if($4=="+" && length($5)>=23) print $1,$2-1,$3,$6","$7","norm/1}' >${outdir}/${normbed%.norm.bed.gz}.bed.plus.pirna 
-zcat $normbed |sed 1d | awk -F "\t" -v norm=$normFac '{OFS="\t"; if($4=="-" && length($5)>=23) print $1,$2-1,$3,$6","$7","norm/1}' >${outdir}/${normbed%.norm.bed.gz}.bed.plus.pirna 
+grep cis $input | awk 'BEGIN{OFS="\t"}{split($7,a,","); if(a[3]=="+"){start=a[2]+10;strand="-";  print a[1],start-1,start,$9,$5*$9,strand} else {start=a[2]-10;strand="+";print a[1],start,start+1,$9,$5*$9,strand}}' | gsort -k1,1 -k2,2 -k3,3 -k6,6 -T $PWD --parallel=8 | bedtools groupby -i stdin -g 1,2,3,6 -c 4,5 -o sum,sum | gsort -k 6,6rn -T $PWD --parallel=8 >${outdir}/${input}.cis.bed &
 
 
-###bedItemOverlapCountWithScore does not work on zlab3
-bedtools sort -i ${outdir}/${normbed%.norm.bed.gz}.bed.plus.pirna | bedItemOverlapCountWithScore dm3 stdin -chromSize=${outdir}/${input}.trans.plus.bed  >${outdir}/${normbed%.norm.bed.gz}.bed.plus.sorted.pirna 
-bedtools sort -i ${outdir}/${normbed%.norm.bed.gz}.bed.minus.pirna | bedItemOverlapCountWithScore dm3 stdin -chromSize=${outdir}/${input}.trans.minus.bed >${outdir}/${normbed%.norm.bed.gz}.bed.minus.sorted.pirna 
 
-
-bedtools sort -i ${outdir}/${normbed%.norm.bed.gz}.bed.plus.pirna | bedItemOverlapCountWithScore dm3 stdin -chromSize=${outdir}/${input}.cis.plus.bed  >${outdir}/${normbed%.norm.bed.gz}.bed.cis.sorted.pirna 
 
