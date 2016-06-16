2bab6337501c8a52ded6057307663fc308ccde8b
turningA <wei.wang2@umassmed.edu>
2016-06-16 13:38:18 -0400

    from zlab4

--- Utils/NNNadaptor.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-adaptor_trimmer_phred_dumper $1 TGGAATTC 0 18 5 | awk -v trimlen=3 '{print $0; getline; print substr($1,1,length($1)-trimlen); getline; print "+"; getline; print substr($1,1,length($1)-trimlen);}' | gzip > ${1%fq}trimmed.fq.gz
-gzip $1
